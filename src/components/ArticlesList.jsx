a7f515e240dfef9ae5cb7369f91ca3418a6a7747
kenchan0824 <kenchan0824@gmail.com>
2023-04-17 15:51:13 +0100

    add loading state

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -4,21 +4,27 @@ import ArticleCard from "./ArticleCard";
 
 function ArticleList() {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
   
   useEffect(() => {
+    setLoading(true);
     getArticles().then((articles) => {
       setArticles(articles);
+      setLoading(false);
     });
   }, []);
 
+  if (loading) {
+    return "loading ...";
+  }
+  
   return (
     <div>
       {articles.map((article) => {
         return <ArticleCard key={article.article_id} article={article} />;
       })}
     </div>
-
-  )
+  );
 }
 
 export default ArticleList;
