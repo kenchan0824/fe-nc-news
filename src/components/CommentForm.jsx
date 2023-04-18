18fae2b8288463610dbe65af89a0fd68f2a80c5f
kenchan0824 <kenchan0824@gmail.com>
2023-04-18 16:46:35 +0100

    post comment optimistically

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -4,6 +4,7 @@ import { postComment } from "../api";
 
 function CommentForm({ user, article_id, setComments }) {
   const [comment, setComment] = useState("");
+  const [error, setError] = useState(false);
 
   function handleChange(event) {
     setComment(event.target.value);
@@ -16,24 +17,43 @@ function CommentForm({ user, article_id, setComments }) {
       return alert("You must type somethings!")
     }
     setComment("");
+    setComments((current) => {
+      return [{body, author: user.username}, ...current]
+    });
+    setError(false);
     postComment(article_id, body, user.username)
       .then((newComment) => {
         setComments((current) => {
-          return [newComment, ...current]
+          return [newComment, ...current.slice(1)];
+        });
+    })
+    .catch(() => {
+        setError(true);
+        setComments((current) => {
+          return current.slice(1);
         });
       });
   }
 
   return (
-    <form className="comment-form" onSubmit={handleSubmit}>
-      <textarea 
-        className="comment-input" 
-        placeholder="type something ..."
-        onChange={handleChange}
-        value={comment}
-      />
-      <button className="comment-button">Post</button>
-    </form>
+    <div className="comment-form" >
+      <form onSubmit={handleSubmit}>
+        <textarea 
+          className="comment-input" 
+          placeholder="type something ..."
+          onChange={handleChange}
+          value={comment}
+        />
+        <button className="comment-button">Post</button>
+      </form>
+      {
+        error && 
+        <div className="tooltip">
+          <span>❗</span> 
+          <span className="tooltip-text">Oops, cannot post your comment!</span>
+        </div>
+      }
+    </div>
   );
 }
 
